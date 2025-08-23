2b29793baba53b6a18cd7655311273fbd769c740
Leandro <92264085+leandromacedosilva@users.noreply.github.com>
2025-08-23 09:47:20 -0300

    Game repository, service, controller
    Game repository, service, controller added in project

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,5 +1,50 @@
 package com.devsuperior.dslist.dto;
 
-public class GameMinDTO {
+import com.devsuperior.dslist.entities.Game;
 
+public class GameMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+	
+	public GameMinDTO() {}
+
+	public GameMinDTO(Game entity) {
+		super();
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+		longDescription = entity.getLongDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+	
+	
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-//import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -16,10 +15,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll() {
+	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-		return dto;
+		
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		//List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		//return dto;
 	}
 
 }
